e7a06c76eb66d867b7485affe01d994cd1c9f085
Don Hilligoss <DonH@dienen.com>
2023-03-16 16:38:22 -0400

    commit changes

--- tictactoe.py
+++ tictactoe.py
@@ -16,17 +16,11 @@ def choose_next(player):
     
 def display_board(board):
     
-    #print(f'   |   |   ')
     print(f' {board[7]} | {board[8]} | {board[9]} ')
-    #print(f'   |   |   ')
     print(f'-----------')
-    #print(f'   |   |   ')
     print(f' {board[4]} | {board[5]} | {board[6]} ')
-    #print(f'   |   |   ')
     print(f'-----------')
-    #print(f'   |   |   ')
     print(f' {board[1]} | {board[2]} | {board[3]} ')
-    #print(f'   |   |   ')
 
 def player_input():
     marker = ''
